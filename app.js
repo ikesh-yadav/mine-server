25ec4f54f36eff5e7aa2ab0e82e0bafd3d42f7a7
ikesh-yadav <ikeshyadav1@gmail.com>
2020-08-10 17:57:29 +0530

    using same port for socket and server

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
-var io = require('socket.io').listen(8080),
-	proc = require('child_process');
+//var io = require('socket.io').listen(8080);
+let proc = require('child_process');
 
 const cors = require('cors');
 const express = require("express");
 const path = require("path");
-const { exit } = require('process');
-const winston = require('winston');
-
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-  ],
-});
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.simple(),
-  }));
-}
-
-
 
 const app = express();
+const server = require('http').createServer(app);
+const options = { /* ... */ };
+const io = require('socket.io')(server, options);
 
 const port = process.env.PORT || 3000;
-let server = null;
 let status = 'Stopped';
 let mc_server = null;
 let console_stuff = [];
@@ -47,8 +21,7 @@ app.use(express.static(path.join(__dirname, "/static/")));
 
 app.get("/", (req, res) => { res.sendFile(path.join(__dirname, "/static", "/mp.html")); } );
 
-
-app.listen(port, (err) => {
+server.listen(port, (err) => {
 	if(err) {console.log(err);return;}
     console.log("Server started at port: "+port)
 })
@@ -57,23 +30,25 @@ io.on('start_server', () => {
 	console.log('start server random input');
 });
 
-io.sockets.on('connection', function(socket) {
+io.on('connection', function(socket) {
 
+	/*
 	servers = {single: "Single Player"}
 
 	socket.on('get_server_list', function(){
 		socket.emit('server_list', servers);
 	});
+	*/
 
 	socket.on('get_status', function(){
 		socket.emit('status', status);
 	});
 
 	// When the client says to start a server...
-	socket.on('start_server', function(name) {
+	socket.on('start_server', () => {
 		
 		// If a server is already running or server doesn't exist
-		if (mc_server || !servers[name]) {
+		if (mc_server ) {
 			// Let the user know that it failed.
 			socket.emit('fail', 'start_server');
 			// Stop execution of this callback
@@ -84,7 +59,6 @@ io.sockets.on('connection', function(socket) {
 
 		//process.stdout.write('start server request recieved');
 		// Set which server is currently running
-		server = name;
 
 		//Change status of server to starting
 		status = 'Starting' ;
@@ -94,10 +68,7 @@ io.sockets.on('connection', function(socket) {
 		mc_server = proc.spawn(
 			// Uses Java to run the server from a command line child process. The command prompt will not be visible.
 			"java",
-			['-Xms1024M', '-Xmx1024M', '-jar', 'server.jar', 'nogui']//,
-			// CWD should contain the folder that contains all of your servers. The _ is just there because I prefix all of my server folders with _
-			// Also not the \ to escape spaces. This is necessary (at least on Windows)
-			//{ cwd: "C:/Program\ Files\ (x86)/Minecraft/_" + servers[server] }
+			['-Xms1024M', '-Xmx1024M', '-jar', 'server.jar', 'nogui']
 		);
 
 		// status = 'Starting' ;
@@ -123,7 +94,7 @@ io.sockets.on('connection', function(socket) {
 		});
 
 		mc_server.on('exit', function () {
-			mc_server = server = null;
+			mc_server = null;
 			io.sockets.emit('status', 'Stopped');
 		});
 
